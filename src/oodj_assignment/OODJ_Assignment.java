f5522b69e92a255c268edb24cc3bce2c6933a6c1
adamc <adamc@ADam-Laptop>
2023-08-17 00:25:13 +0800

    i add fuck u

--- src/oodj_assignment/OODJ_Assignment.java
+++ src/oodj_assignment/OODJ_Assignment.java
@@ -8,6 +8,7 @@ public class OODJ_Assignment {
     public static void main(String[] args) {
         System.out.println("hdll");
         System.out.println("test");
+        System.out.println("fuck u");
     }
     
 }
