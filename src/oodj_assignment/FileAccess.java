a648f7a2c229de7536c8302b5deb590394007dc4
drag-0130-on <drag.0130.on@gmail.com>
2023-09-02 15:51:58 +0800

    FileAccess

--- src/oodj_assignment/FileAccess.java
+++ src/oodj_assignment/FileAccess.java
@@ -23,7 +23,7 @@ public class FileAccess {
         String line;
         ArrayList<String[]> content = new ArrayList<String[]>();
         while ((line = br.readLine())!= null){
-            content.add(line.split("|"));
+            content.add(line.split("\\|"));
         }
         br.close();
         return content;
@@ -39,7 +39,7 @@ public class FileAccess {
         ArrayList<String[]> content = new ArrayList<String[]>();
         while ((line = br.readLine())!= null){
             if (line.contains(s)){
-                content.add(line.split("|"));
+                content.add(line.split("\\|"));
             }
         }
         br.close();
@@ -53,7 +53,7 @@ public class FileAccess {
             e.printStackTrace();
         }
         ArrayList<String[]> content = viewFile();
-        content.add(data.split("|"));
+        content.add(data.split("\\|"));
         for (String[] line:content){
             bw.write(String.join("|",line));
             bw.newLine();
