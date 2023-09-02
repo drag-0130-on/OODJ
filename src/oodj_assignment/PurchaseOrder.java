d55dc0b3dc8d7291f5d70a00c040cd619e0e68a6
drag-0130-on <drag.0130.on@gmail.com>
2023-09-02 16:17:32 +0800

    FileAccess

--- src/oodj_assignment/PurchaseOrder.java
+++ src/oodj_assignment/PurchaseOrder.java
@@ -6,18 +6,13 @@ import java.util.ArrayList;
 
 public class PurchaseOrder implements Record {
     FileAccess poFile = new FileAccess("pr.txt");
+    String poID;
+    PurchaseRequisition pr;
     
-    String Item_Id,Item_Name,Required_Before,Supplier_Id,Approved_By,Checked_By;
-    int Quantity;
+   
     
-    public PurchaseOrder(String Item_Id,String Item_Name,String Required_Before,int Quantity,String Supplier_Id,String Approved_By,String Checked_By){
-        this.Item_Id = Item_Id;
-        this.Item_Name = Item_Name;
-        this.Required_Before = Required_Before;
-        this.Quantity = Quantity;
-        this.Supplier_Id = Supplier_Id;
-        this.Approved_By = Approved_By;
-        this.Checked_By = Checked_By;
+    public PurchaseOrder(){
+        
     }
     public void add()throws IOException{
         poFile.addToFile(toString());
@@ -35,6 +30,6 @@ public class PurchaseOrder implements Record {
         return poFile.viewFile(s);
     }
     public String toString(){
-        return Item_Id+"|"+Item_Name+"|"+Required_Before+"|"+Quantity+"|"+Supplier_Id+"|"+Approved_By+"|"+Checked_By;
+        return (poID + "|" + pr.toString());
     }
 }
