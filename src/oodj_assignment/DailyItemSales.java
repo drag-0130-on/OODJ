d2e7c5b4e80636671b0f00406960eabff4aaa6b7
drag-0130-on <drag.0130.on@gmail.com>
2023-09-05 02:22:20 +0800

    Recordfilter function

--- src/oodj_assignment/DailyItemSales.java
+++ src/oodj_assignment/DailyItemSales.java
@@ -6,15 +6,14 @@ import java.util.ArrayList;
 
 public class DailyItemSales implements Record {
     FileAccess dailyFile = new FileAccess("daily.txt");
-    String Item_Id,Item_Name,Date,Supplier_Id;
-    int Amount_Sold;
+    String date;
+    int amountSold;
+    Item item;
     
-    public DailyItemSales(String Item_Id,String Item_Name, int Amount_Sold,String Date,String Supplier_Id){
-        this.Item_Id = Item_Id;
-        this.Item_Name = Item_Name;
-        this.Amount_Sold = Amount_Sold;
-        this.Date = Date;
-        this.Supplier_Id = Supplier_Id;
+    public DailyItemSales(String date, int amountSold, Item item){
+        this.date = date;
+        this.amountSold = amountSold;
+        this.item = item;
     }   
     public void add() throws IOException{
         dailyFile.addToFile(toString());
@@ -36,6 +35,7 @@ public class DailyItemSales implements Record {
         return dailyFile.viewFileWithFilter(AL,attIndex,filter);
     }
     public String toString(){
-        return Item_Id+"|"+Item_Name+"|"+Date+"|"+Amount_Sold+"|"+Supplier_Id;
+        return (date + "|" + amountSold + "|" + item.toString());
     }
 }
+                                                                    
