e8ef25caf4a79b82d7da69b6417bcb735c4322c0
drag-0130-on <drag.0130.on@gmail.com>
2023-09-07 01:27:53 +0800

    usemethods....

--- src/oodj_assignment/FileAccess.java
+++ src/oodj_assignment/FileAccess.java
@@ -14,14 +14,15 @@ public class FileAccess {
         this.fileName = fileName;
     }
     public ArrayList<String[]> viewFile() throws IOException{
+        ArrayList<String[]> content = new ArrayList<String[]>();
+        String line;
         try{
             br = new BufferedReader(new FileReader(new File(fileName)));
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        String line;
-        ArrayList<String[]> content = new ArrayList<String[]>();
+       
         while ((line = br.readLine())!= null){
             content.add(line.split("\\|"));
         }
@@ -48,13 +49,14 @@ public class FileAccess {
         return content;
     }
     public boolean addToFile(String data) throws IOException{
+        ArrayList<String[]> content = viewFile();
         try{
             bw = new BufferedWriter(new FileWriter(new File(fileName)));
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        ArrayList<String[]> content = viewFile();
+        
         content.add(data.split("\\|"));
         for (String[] line:content){
             bw.write(String.join("|",line));
@@ -64,13 +66,13 @@ public class FileAccess {
         return true;
     }
     public boolean editFile(String oldData, String newData) throws IOException{
+        ArrayList<String[]> content = viewFile();
         try{
             bw = new BufferedWriter(new FileWriter(new File(fileName)));
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        ArrayList<String[]> content = viewFile();
         for (String[] line:content){
             if (oldData.equals(String.join("|", line))){
                 bw.write(newData);
@@ -84,13 +86,13 @@ public class FileAccess {
         return true;
     }
     public boolean removeFromFile(String data) throws IOException{
+        ArrayList<String[]> content = viewFile();
         try{
             bw = new BufferedWriter(new FileWriter(new File(fileName)));
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        ArrayList<String[]> content = viewFile();
         for (String[] line:content){
             if (!(data.equals(String.join("|",line)))){
                 bw.write(String.join("|",line));
